075281e38879525f50b8f3d29f9f22fd61c857dc
jiaoqiqi <1176932165@qq.com>
2018-04-07 16:07:29 +0800

    add comment
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,8 +1,21 @@
 import React,{Component} from 'react';
 class CommentList extends Component{
     render(){
+        const comments = [
+            {username:"tony",content : "hello"},
+            {username:"pick",content : "world"},
+            {username:"haha",content : "hhhhhh"}
+        ];
         return(
-            <div>CommentList</div>
+           <div>
+               {comments.map((comment,i) => {
+                   return(
+                       <div key={i}>
+                           {comment.username} : {comment.content};
+                       </div>
+                   )
+               })}
+           </div>
         )
     }
 }
